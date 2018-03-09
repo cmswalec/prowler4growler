ac778215784a845aab3bf4c5271c44c6d5771fc8
cmswalec <cmswalec@wpi.edu>
2018-03-08 19:39:07 -0800

    Data wrangled for CA

--- beer_city_wrangling.R
+++ beer_city_wrangling.R
@@ -1,18 +1,18 @@
 
+library(tidyverse)
 
-beers <- read.csv("beers.csv")
-brews <- read.csv("breweries.csv")
-
+beers <- read_csv("beers.csv")
+brews <- read_csv("breweries.csv")
 
 full_beer_list <- merge(beers, brews)
-full_beer_list$city_state <- paste(full_beer_list$city, full_beer_list$state)
+CA <- full_beer_list %>% 
+  filter(state == "CA")
 
-write.csv(full_beer_list, "full_beer_list.csv")
+cities <- read_csv("uscitiesv1.3.csv")
+cities2 <- cities %>% 
+  filter(state_id_x == "CA")
 
+CA_beer <- merge(CA, cities2, by.x = "city", by.y = "city_x") %>% 
+  select("city", "abv", "ibu", "name", "style", "brew_name", "lat", "lng")
 
-cities <- read.csv("uscitiesv1.3.csv")
-#cities$city_state <- paste(cities$city_x, cities$state_id_x)
-
-cities2 <- unite(cities, newcol, c(city_x, state_id_x), remove = FALSE)
-
-list_with_spatial <- merge(full_beer_list, cities, by.x = 'city', by.y = 'city_x')
+write.csv(CA_beer, "CA_beer.csv")
